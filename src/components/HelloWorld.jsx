4ca4fa94cb028f0013bfb809cbfc3f364cf7dc72
balbini <balbini91@gmail.com>
2018-04-04 21:48:50 -0700

    added description of project and tutorial link

--- src/components/HelloWorld.jsx
+++ src/components/HelloWorld.jsx
@@ -4,7 +4,9 @@ export class HelloWorld extends Component {
   render (){
     return (
       <div className="hello-world">
-        <h1>Hello World!</h1>
+        <h1>My React App With Build Tools</h1>
+        <a href="https://medium.com/front-end-hacking/what-are-npm-yarn-babel-and-webpack-and-how-to-properly-use-them-d835a758f987"><p>Tutorial</p></a>
+        <p>This is a small React app that I built to learn how to work with build tools. It was built with yarn, babel, and webpack. The tutorial I followed to learn these skills is linked above.</p>
       </div>
     );
   }
